7abf5a0be5a697d171dc2838b6e79a510221760c
Felix Kurz <115644420+BrrrUzi@users.noreply.github.com>
2025-06-07 18:42:17 +0200

    Verbessere Chat-Komponenten: Füge mobile Unterstützung hinzu, um die Anzeige von Chats und Chatbereichen basierend auf der Bildschirmgröße zu optimieren. Implementiere die Funktion zum Zurücksetzen des ausgewählten Chats.

--- src/app/(private)/chat/chat-components/chat-area/recipient.tsx
+++ src/app/(private)/chat/chat-components/chat-area/recipient.tsx
@@ -5,17 +5,23 @@ import { useSelector } from 'react-redux'
 import RecipientInfo from './recipient-info'
 import { ChatType } from '@/interfaces'
 import socket from '@/config/socket-config'
+import { ArrowLeft } from 'lucide-react'
+import { clearSelectedChat } from '@/redux/chatSlice'
+import { useDispatch } from 'react-redux'
 
 
-export default function Recipient() {
+
+export default function Recipient({ isMobile }: { isMobile: boolean }) {
     const [typing = false, setTyping] = React.useState<boolean>(false)
     const [senderName = "", setSenderName] = React.useState<string>("")
     const [showRecipientInfo, setShowRecipientInfo] = React.useState<boolean>(false)
     const { selectedChat }: ChatState = useSelector((state: any) => state.chat)
     const { currentUserData }: UserState = useSelector((state: any) => state.user)
+    const dispatch = useDispatch()
 
     let chatName = ""
     let chatImage = ""
+
 
     if (selectedChat?.isGroupChat) {
         chatName = selectedChat.groupName
@@ -58,18 +64,27 @@ export default function Recipient() {
     return (
         <div className='flex justify-between items-center px-5 py-3 border-b border-gray-200 bg-gray-300 w-full cursor-pointer'>
 
-            <div className='flex gap-5 items-center' onClick={() => setShowRecipientInfo(true)}>
-                <img
-                    src={chatImage || './image.png'}
-                    alt="Profile"
-                    className='w-10 h-10 rounded-full'
-                    onError={(e) => {
-                        (e.target as HTMLImageElement).src = './image.png';
-                    }}
-                />
-                <div className='flex flex-col gap-1'>
-                    <span className='text-gray-700 text-sm'>{chatName}</span>
-                    {typingAnimation()}
+            <div className='flex items-center gap-4'>
+                {isMobile && (
+                    <ArrowLeft
+                        className="w-5 h-5 text-gray-600 cursor-pointer"
+                        onClick={() => dispatch(clearSelectedChat())}
+                    />
+                )}
+
+                <div className='flex gap-3 items-center' onClick={() => setShowRecipientInfo(true)}>
+                    <img
+                        src={chatImage || './image.png'}
+                        alt="Profile"
+                        className='w-10 h-10 rounded-full'
+                        onError={(e) => {
+                            (e.target as HTMLImageElement).src = './image.png';
+                        }}
+                    />
+                    <div className='flex flex-col gap-1'>
+                        <span className='text-gray-700 text-sm'>{chatName}</span>
+                        {typingAnimation()}
+                    </div>
                 </div>
             </div>
 
@@ -78,3 +93,4 @@ export default function Recipient() {
             )}
         </div>
     )
+}
--- src/app/(private)/chat/page.tsx
+++ src/app/(private)/chat/page.tsx
@@ -1,13 +1,29 @@
 "use client";
 import Chats from "./chat-components/chats";
 import ChatArea from "./chat-components/chat-area";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import { ChatState } from "@/redux/chatSlice";
+import { useSelector } from "react-redux";
 
 export default function ({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isPublicRoute = pathname.includes("sign-in") || pathname.includes("sign-up");
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  const { selectedChat }: ChatState = useSelector((state: any) => state.chat);
+
   if (isPublicRoute) return <>{children}</>;
+
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth <= 1020);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
 
   useEffect(() => {
     document.body.style.overflow = 'hidden';
@@ -15,13 +31,17 @@ export default function ({ children }: { children: React.ReactNode }) {
       document.body.style.overflow = '';
     };
   }, []);
+  
+  if (isMobile === null) return null;
+  const showChatsOnly = isMobile && !selectedChat;
+  const showChatOnly = isMobile && selectedChat;
+  const showBoth = !isMobile;
 
   return (
     <div className="flex h-full w-full overflow-hidden">
-      <Chats />
-      <div className="w-px h-full bg-gray-300" />
-      <ChatArea />
-
+      {(showChatsOnly || showBoth) && <Chats fullscreen={showChatsOnly} />}
+      {showBoth && <div className="w-px h-full bg-gray-300" />}
+      {(showChatOnly || showBoth) && <ChatArea isMobile={isMobile} />}
     </div>
   );
 }
