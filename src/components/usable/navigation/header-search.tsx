7fe109cc9cb9b579dcc0e0686939835a959f9a9a
ditchbyers <may-pascalrene@web.de>
2025-06-20 19:40:54 +0200

    asd

--- src/components/usable/navigation/header-search.tsx
+++ src/components/usable/navigation/header-search.tsx
@@ -89,7 +89,7 @@ export default function SearchCommandPopover() {
   }
 
   return (
-    <div className="mx-auto w-full max-w-md">
+    <div className="mx-auto w-full">
       <div className="relative" ref={containerRef}>
         <div className="relative">
           <Search className="text-muted-foreground absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 transform" />
