07e9cebe8e462c3ec0e89c8c86d3d3823a5dfd7f
ditchbyers <may-pascalrene@web.de>
2025-06-20 15:50:09 +0200

    asdf

--- src/components/usable/chat/chat-area/message.tsx
+++ src/components/usable/chat/chat-area/message.tsx
@@ -16,7 +16,9 @@ export default function Message({ message }: { message: MessageType }) {
       <div className="flex justify-end gap-2">
         <div className="flex flex-col items-end gap-1">
           <span className="text-xs text-gray-500">You</span>
-          <p className="bg-primary m-0 rounded-xl rounded-tr-none px-7 py-2 text-sm text-white">{message.text}</p>
+          <p className="bg-primary m-0 rounded-xl rounded-tr-none px-7 py-2 text-sm tracking-wide wrap-anywhere text-white">
+            {message.text}
+          </p>
           <span className="text-xs text-gray-500">{formatDateTime(message.createdAt)}</span>
         </div>
         <img src={message.sender.profilePicture} alt="avatar" className="h-6 w-6 rounded-full" />
@@ -28,7 +30,9 @@ export default function Message({ message }: { message: MessageType }) {
         <img src={message.sender.profilePicture} alt="avatar" className="h-6 w-6 rounded-full" />
         <div className="flex flex-col gap-2">
           <span className="text-xs text-gray-500">{message.sender.name}</span>
-          <p className="text-primary m-0 overflow-x-hidden rounded-xl rounded-tl-none bg-gray-200 px-7 py-2 text-sm">{message.text}</p>
+          <p className="text-primary m-0 overflow-x-hidden rounded-xl rounded-tl-none bg-gray-200 px-7 py-2 text-sm tracking-wide wrap-anywhere">
+            {message.text}
+          </p>
           <span className="text-xs text-gray-500">{formatDateTime(message.createdAt)}</span>
         </div>
       </div>
